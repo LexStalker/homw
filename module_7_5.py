c45c2818684ebae057be6689a9297d340178dd4a
LexStalker <jamboraja@gmail.com>
2024-10-14 15:12:48 +0400

    module_7_5_new

--- module_7_5.py
+++ module_7_5.py
@@ -1,13 +1,15 @@
 import os
 import time
-directory = '.'
-print(list(os.walk((directory))))
+directory = "."
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = __file__
-        filetime = os.path.getmtime(filepath)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(__file__)
-        parent_dir = os.path.dirname(__file__)
-print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
-              f'Родительская директория: {parent_dir}')
+        file_path = os.path.join(root, file)
+        last_modified_time = os.path.getmtime(file_path)
+        file_size = os.path.getsize(file_path)
+        parent_directory = os.path.dirname(file_path)
+print(f"File: {file}")
+print(f"Full path: {file_path}")
+print(f"Last modified: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(last_modified_time))}")
+print(f"File size: {file_size} bytes")
+print(f"Parent directory: {parent_directory}")
+print()

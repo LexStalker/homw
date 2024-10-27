1a0886320bb887a08a07be7d596907a14099788a
LexStalker <jamboraja@gmail.com>
2024-10-27 12:13:34 +0400

    module_9_4_new

--- module_9_4.py
+++ module_9_4.py
@@ -1,23 +1,25 @@
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
+compare = map(lambda x, y: list(x) == list(y), first, second)
+print(list(compare))
 
-coincidence = map(lambda x, y: list(x) == list(y), first, second)
-print(list(coincidence))
-
-def  get_advanced_writer(file_name):
+def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        with open(file_name, 'w') as f:
-            for x in data_set: f.write(f'{x}\n')
-    return write_everything()
+        with open(file_name, "w") as f:
+            for x in data_set: f.write(f"{x}\n")
+
+    return write_everything
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-with open(exemple.txt, 'r') as f: print(f.read())
+with open('example.txt', "r") as f: print(f.read())
 
 class MysticBall:
     from random import choice
+
     def __init__(self, *words):
         self.words = words
+
     def __call__(self):
         return self.choice(self.words)
 

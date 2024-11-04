016651f462429a11b424a9c762068d8e8b8d4b41
LexStalker <jamboraja@gmail.com>
2024-11-04 11:30:46 +0400

    module_10_4

---
+++ module_10_4.py
@@ -0,0 +1,78 @@
+from threading import Thread
+import queue
+import random
+from time import sleep
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+
+class Guest(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.target = self.run
+
+    def run(self):
+        sleep(random.randint(3, 10))
+
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = queue.Queue()
+        self.tables = [table for table in tables]
+
+    def guest_arrival(self, *guests):
+        # прибытие гостей
+        for guest in guests:
+            if any(table.guest is None for table in self.tables):
+                # если есть свободный стол ищем его и сажаем гостя за него
+                for table in self.tables:
+                    if table.guest == None:
+                        table.guest = guest
+                        print(f'{guest.name} сел(-а) за стол номер {table.number}')
+                        guest.start()
+                        break
+            else:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+
+    def discuss_guests(self):
+        # обслужить гостей
+        while not self.queue.empty() or any(table.guest for table in self.tables):
+            for table in self.tables:
+                if table.guest:
+                    if table.guest.is_alive(): # посетитель ест
+                        continue
+                    else:
+                        print(f'{table.guest.name} поел(-а) и ушёл(ушла)')
+                        print(f"Стол номер {table.number} свободен")
+                        table.guest = None
+                        if not self.queue.empty():  # если ещё есть посетители
+                            guest = self.queue.get()
+                            table.guest = guest
+                            print(f'{guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                            guest.start()
+
+        print(f'Гостей нет, кафе закрыто.')
+
+
+if __name__ == "__main__":
+    # Создание столов
+    tables = [Table(number) for number in range(1, 6)]
+    # Имена гостей
+    guests_names = [
+        'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+        'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+    ]
+    # Создание гостей
+    guests = [Guest(name) for name in guests_names]
+    # Заполнение кафе столами
+    cafe = Cafe(*tables)
+    # # Приём гостей
+    cafe.guest_arrival(*guests)
+    # # Обслуживание гостей
+    cafe.discuss_guests()
